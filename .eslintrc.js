e940a56f50e3975ec76642313c5e888bb4c38687
Yano <Yano@localhost>
2022-10-30 20:22:01 +0900

    Support TypeScript

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,12 @@ module.exports = {
     es2021: true,
   },
   plugins: ['react', 'react-hooks', 'prettier'],
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
